6374d68fd9ec90746b0e212e642d9aa101e6d29a
BUTELLE Dylan <butelledylan@gmail.com>
2021-05-06 11:07:24 +0200

    ajout site pour heroku

---
+++ contact.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <?php include "addons/header.html";?>
+    <title>Me contacter | Dylan Butelle</title>
+</head>
+<body>
+<article>
+    <div id="contact">
+        <div class="container">
+
+            <h1>Contact</h1>
+            <h2>Gardons contact</h2>
+
+            <h3>Adresse email</h3>
+            <p>
+                <a href="mailto:butelledylan@gmail.com">
+                    butelledylan@gmail.com
+                </a>
+            </p>
+
+            <h3>M'écrire</h3>
+            <p>
+                Q&D Informatique<br/>
+                55 Bis rue de la courtille<br/>
+                Equihen-Plage, 62224, France
+            </p>
+
+        </div>
+    </div>
+
+    <iframe style="margin-bottom: -7px; width: 100%" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2528.097462736762!2d1.5731183159236235!3d50.68101587860525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dc2ca7908a8f01%3A0x21ae7cb2e130bd3d!2s55%20Rue%20de%20la%20Courtille%2C%2062224%20%C3%89quihen-Plage!5e0!3m2!1sfr!2sfr!4v1619867292224!5m2!1sfr!2sfr" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+</article>
+</body>
+<footer>
+    <?php include "addons/footer.html";?>
+</footer>
+
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <?php include "addons/header.html";?>
+        <title>Home | Dylan Butelle</title>
+    </head>
+    <body>
+    <article>
+        <div id="home">
+            <div class="container">
+
+                <h1>Bonjour ! Je suis Dylan</h1>
+                <h2>Je suis étudiant en informatique</h2>
+                <h3>Bienvenue sur mon site personnel.</h3>
+            </div>
+        </div>
+
+    </article>
+    </body>
+    <footer>
+        <?php include "addons/footer.html";?>
+    </footer>
+
+
+
+</html>
