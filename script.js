d126621d087d68778bb6fd749be60e6970c9948c
BUTELLE Dylan <butelledylan@gmail.com>
2021-06-25 19:54:17 +0200

    ajout personnage white/dark mode

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded",function(){
     let darkMode = document.getElementById("button")
+    let me = document.getElementById("me")
     let body = document.body;
     darkMode.addEventListener("click",function(){
         body.classList.toggle("dark");
+        if(!body.classList.contains("dark")){
+            me.setAttribute("src","downloads/black.png")
+        }else{
+            me.setAttribute("src","downloads/white.png")
+        }
     })
 })
